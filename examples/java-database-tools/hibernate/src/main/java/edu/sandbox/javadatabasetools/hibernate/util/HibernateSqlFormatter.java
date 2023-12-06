65ab89b49a4a964fed9317df8692fbd17692d3e8
alpa1479 <alpa1479@gmail.com>
2023-12-06 23:49:51 +0400

    [main] - added HIGHLIGHT Hibernate formatter

--- examples/java-database-tools/hibernate/src/main/java/edu/sandbox/javadatabasetools/hibernate/util/HibernateSqlFormatter.java
+++ examples/java-database-tools/hibernate/src/main/java/edu/sandbox/javadatabasetools/hibernate/util/HibernateSqlFormatter.java
@@ -1,18 +1,17 @@
 package edu.sandbox.javadatabasetools.hibernate.util;
 
 import com.p6spy.engine.spy.appender.MessageFormattingStrategy;
-import org.hibernate.engine.jdbc.internal.BasicFormatterImpl;
-import org.hibernate.engine.jdbc.internal.Formatter;
+import org.hibernate.engine.jdbc.internal.FormatStyle;
 
 import static java.lang.String.format;
 import static org.springframework.util.StringUtils.hasLength;
 
 public class HibernateSqlFormatter implements MessageFormattingStrategy {
 
-    private static final Formatter HIBERNATE_SQL_FORMATTER = new BasicFormatterImpl();
-
     @Override
     public String formatMessage(int connectionId, String now, long elapsed, String category, String prepared, String sql, String url) {
-        return hasLength(sql) ? format("Hibernate: %s {elapsed: %dms}", HIBERNATE_SQL_FORMATTER.format(sql), elapsed) : "";
+        return hasLength(sql) ?
+                format("Hibernate:%n %s {elapsed: %dms}%n",
+                        FormatStyle.HIGHLIGHT.getFormatter().format(FormatStyle.BASIC.getFormatter().format(sql)), elapsed) : "";
     }
 }
